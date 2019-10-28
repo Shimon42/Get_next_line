c1c99312777bcac9920c12b128c19900cf00d567
Shimon42 <pixono26@gmail.com>
2019-10-28 23:50:08 +0100

    Update get_next_line.c
    Still not good
    Better get brain body
    Init new brain in initbrain
    
    messy tests function that can be usefull

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: siferrar <siferrar@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/10/21 20:22:38 by siferrar     #+#   ##    ##    #+#       */
-/*   Updated: 2019/10/27 20:16:20 by siferrar    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/10/28 23:48:59 by siferrar    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -30,6 +30,28 @@ void disp_brain(t_gnl b)
 	nbr_read: %d\n\
 	init: %d\n\
 	check: %d\n", b.fd, b.line, b.buff, b.left, b.asleft, b.line_len, b.nbr_read, b.init, b.check);
+}
+
+t_gnl	*init_brain(int fd, char **line)
+{
+	t_gnl *brain;
+	printf("-- Init brain fd %d\n", fd);
+	if ((brain = malloc(sizeof(t_gnl))) != NULL)
+	{
+		brain->asleft = 0;
+		brain->init = 1;
+		brain->fd = fd;
+		brain->next = NULL;
+		brain->line = *line;
+		if ((brain->buff = malloc(BUFFER_SIZE * sizeof(char))) == NULL)
+			return (NULL);
+		brain->nbr_read = 0;
+		brain->line_len = 0;
+		brain->check = -1;
+		//disp_brain(*brain);
+		printf("-- NEW FD OK %d\n", fd);
+	}
+	return (brain);
 }
 
 int	treat_left(t_gnl *b)
@@ -67,62 +89,36 @@ int	treat_read(t_gnl *b)
 	return (1);
 }
 
-int treat_all(t_gnl *b)
+t_gnl	*get_brain(t_gnl *b, int fd, char **line)
 {
-	if ((*b).left)
-		if (((*b).check = has_eol((*b).left)))
-		{
-			ft_strlcpy((*b).line, (*b).left + (*b).check, ft_strlen((*b).left) - (*b).check);
-			return (1);
-		}
-	return (1);
-}
+	printf("-- Searching for Fd %d\n", fd);
+	t_gnl **ptr;
 
-t_gnl	*init_brain(t_gnl *brain, int fd, char **line)
-{
-	if (!(*brain).init)
+	ptr = &b;
+
+	if((*ptr))
 	{
-		(*brain).asleft = 0;
-		(*brain).init = 1;
-		(*brain).fd = fd;
-		(*brain).next = NULL;
-	}
-	(*brain).line = *line;
-	if (((*brain).buff = malloc(BUFFER_SIZE * sizeof(char))) == NULL)
-		return (NULL);
-	(*brain).nbr_read = 0;
-	(*brain).line_len = 0;
-	(*brain).check = -1;
-	printf("-- NEW FD OK %d\n", fd);
-	return (brain);
-}
-
-t_gnl *get_brain(t_gnl *b, int fd, char **line)
-{
-	t_gnl *newfd;
-
-	newfd = b;
-	printf("-- SEARCH FD %d\n", fd);
-	if (newfd)
-	{
-		while (newfd->next)
+		while ((*ptr)->next)
 		{
-			disp_brain(*newfd);
-			if (newfd->fd == fd)
+			printf("-- \033[1;33mtmpid %d\033[0m\n", b->fd);
+			if((*ptr)->fd == fd)
 			{
-				printf("-- FOUND FD %d\n", fd);
-				newfd = init_brain(newfd, fd, line);
-				return (newfd);
+				printf("\033[1;33m-- Found !\033[1;33m\n");
+				return ((*ptr));
 			}
-			newfd = newfd->next;
+			(*ptr) = (*ptr)->next;
 		}
+		(*ptr)->next = init_brain(fd, line);
+		return ((*ptr)->next);
 	}
-	printf("-- NEW FD %d\n", fd);
-	newfd = init_brain(newfd, fd, line);
-	(*newfd).next = b;
-	return (b);
-		
+	printf("-- Not Found ...\n");
+	printf("-- FIRST IN LIST\n");
+	b = init_brain(fd, line);
+	printf("Malloc ok\n");
+	printf("-- Returning new fd %d\n", fd);
+	return ((*ptr));
 }
+
 /*
 int get_next_lineGG(int fd, char **line)
 {
@@ -144,15 +140,29 @@ int get_next_lineGG(int fd, char **line)
 	return (0);
 }*/
 
+int treat_all(t_gnl *b)
+{
+	if ((*b).left)
+		if (((*b).check = has_eol((*b).left)))
+		{
+			ft_strlcpy((*b).line, (*b).left + (*b).check, ft_strlen((*b).left) - (*b).check);
+			return (1);
+		}
+	return (1);
+}
+
 int get_next_line(int fd, char **line)
 {
-	static	t_gnl b;
-	t_gnl *curb;
+	static	t_gnl *b;
+	t_gnl **curb;
 
+	curb = &b;
+	ft_lstiter(b);
 	if (BUFFER_SIZE > 0)
-		if ((curb = get_brain(&b, fd, line)))
+		if (get_brain(*curb, fd, line))
 			{
-				while (treat_left(curb) && (*curb).check < 0)
+				//disp_brain(curb);
+				/*while (treat_left(curb) && (*curb).check < 0)
 				{
 					if (((*curb).nbr_read = read(fd, (*curb).buff, BUFFER_SIZE))
 						&& ((*curb).check = has_eol((*curb).buff)) >= 0)
@@ -162,7 +172,7 @@ int get_next_line(int fd, char **line)
 					ft_realloc((*curb).line, 0, ft_strlen((*curb).line) + BUFFER_SIZE);
 					(*curb).line = ft_strjoin((*curb).line, (*curb).buff);
 					*line = (*curb).line;
-				}
+				}*/
 			}
 	return (0);
 }
@@ -178,21 +188,21 @@ int main(void)
 	int res = 1;
 	ptr = &line;
 	//fd = 0;
-	//int fd2 = open("test2.txt", O_CREAT | O_RDWR);
-	//char *line2;
-	//char **ptr2;
-	//int res2 = 1;
-	//ptr2 = &line2;
+	int fd2 = open("test2.txt", O_CREAT | O_RDWR);
+	char *line2;
+	char **ptr2;
+	int res2 = 1;
+	ptr2 = &line2;
 	////res2 = 1;
 	//fd2 = 1;
 	printf("\n\033[1;33m--------------- GNL START ------------------\033[0m\n");
-	while (i < 6)
+	while (i < 2)
 	{
 		printf("\n\033[1;34m--------------- GNL - %d ------------------\033[0m\n\n", i);
 		res = get_next_line(fd, ptr);
-	//	res2 = get_next_line(fd2, ptr2);
-		printf("\n\033[0;32mfd: %d - RES  %d -> %s\033[0;35m[end]\033[0m - RETURN %d\n", fd, i, line, res);
-		//printf("\n\033[0;32mfd: %d - RES %d -> %s\033[0;35m[end]\033[0m - RETURN %d\n", fd2, i, line2, res2);
+		res2 = get_next_line(fd2, ptr2);
+		printf("\n\033[0;32mfd: %d - RES %d -> %s\033[0;35m[end]\033[0m - RETURN %d\n", fd, i, line, res);
+		printf("\n\033[0;32mfd: %d - RES %d -> %s\033[0;35m[end]\033[0m - RETURN %d\n", fd2, i, line2, res2);
 			i++;
 	}
 	printf("\n\033[1;32m--------------- GNL   END ------------------\033[0m\n");
